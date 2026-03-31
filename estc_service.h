41a00f35bbc649e60a6f6d907cf24242e6906d2c
RShaxinya <shaxathrasher@gmail.com>
2026-03-31 14:39:51 +0500

    Update estc_service.h

--- estc_service.h
+++ estc_service.h
@@ -3,32 +3,55 @@
 #define ESTC_SERVICE_H__
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #include "ble.h"
 #include "sdk_errors.h"
 
-#define ESTC_BASE_UUID { 0x89, 0xED, 0x3C, 0x19, 0x84, 0x1A, /* - */ 0x31, 0x97, /* - */ 0x09, 0x42, /* - */ 0x2E, 0x32, /* - */ 0x00, 0x00, 0xA3, 0x3D }
+
+#define ESTC_BASE_UUID { 0x89, 0xED, 0x3C, 0x19, 0x84, 0x1A, /* - */ 0x31, 0x97, /* - */ 0x09, 0x42, /* - */ 0x2E, 0x32, /* - */ 0x00, 0x00, 0xA3, 0x3D } 
 
 #define ESTC_SERVICE_UUID 0x6577
 
+#define ESTC_GATT_CHAR_LED_STATE_UUID 0x6578   
+#define ESTC_GATT_CHAR_LED_COLOR_UUID 0x6579   
 
-#define ESTC_GATT_CHAR_1_UUID 0x6578
+typedef enum {
+    BLE_ESTC_EVT_LED_STATE_WRITE,
+    BLE_ESTC_EVT_LED_COLOR_WRITE
+} ble_estc_evt_type_t;
 
-typedef struct
+typedef struct {
+    ble_estc_evt_type_t evt_type;
+    union {
+        uint8_t  led_state; 
+        uint32_t led_color; 
+    } params;
+} ble_estc_evt_t;
+
+struct ble_estc_service_s;
+typedef void (*ble_estc_evt_handler_t)(struct ble_estc_service_s *service, ble_estc_evt_t *evt);
+
+typedef struct ble_estc_service_s
 {
     uint16_t service_handle;
     uint16_t connection_handle;
-    uint8_t uuid_type;
+    uint8_t  uuid_type;
 
-   
-    ble_gatts_char_handles_t char1_handles;
+    ble_gatts_char_handles_t led_state_handles;
+    ble_gatts_char_handles_t led_color_handles;
+
+    bool is_led_state_notifying;
+    bool is_led_color_notifying;
+
+    ble_estc_evt_handler_t evt_handler;
 } ble_estc_service_t;
 
 ret_code_t estc_ble_service_init(ble_estc_service_t *service);
 
 void estc_ble_service_on_ble_event(const ble_evt_t *ble_evt, void *ctx);
 
-void estc_update_characteristic_1_value(ble_estc_service_t *service, int32_t *value);
-
+void estc_update_led_state(ble_estc_service_t *service, uint8_t state, bool update_gatts);
+void estc_update_led_color(ble_estc_service_t *service, uint32_t color, bool update_gatts);
 
 #endif /* ESTC_SERVICE_H__ */
