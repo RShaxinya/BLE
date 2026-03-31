925a8c706ff5defb426737cecbd657b0fb4d8e43
RShaxinya <shaxathrasher@gmail.com>
2026-03-31 14:58:20 +0500

    Update ble_core.h

--- ble/ble_core.h
+++ ble/ble_core.h
@@ -1,1 +1,10 @@
-ble/ble_core.h
+#ifndef BLE_CORE_H__
+#define BLE_CORE_H__
+
+#include <stdint.h>
+#include <stdbool.h>
+
+void ble_core_init(void);
+void ble_core_advertising_start(void);
+
+#endif // BLE_CORE_H__
